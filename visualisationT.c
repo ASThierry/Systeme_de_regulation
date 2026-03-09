604125b0e00e89fefdc7d7c812605b7ee461e5e3
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-03-09 17:31:20 +0100

    Update visualisationT.c

--- visualisationT.c
+++ visualisationT.c
@@ -2,5 +2,34 @@
 
 void visualisationT(temp_t myTemp)
 {
+   char temoin[10];
+   FILE *inputFile;
+   FILE *verrouConsigne;
+   verrouConsigne = fopen(".verrouData","r");
    
+   
+   if (verrouConsigne != NULL){// Présence du verrou
+        fclose( verrouConsigne );
+        return;
+    }else
+    {
+        verrouConsigne = fopen(".verrouData","w");//Création de fichier .        
+        fclose( verrouConsigne );
+
+        inputFile = fopen("data.txt", "r");
+        fscanf(inputFile,"%s",temoin);// Lecture du temoin
+        fclose(inputFile);
+
+        inputFile = fopen("data.txt", "w");
+        if ( inputFile == NULL) { // teste l'ouverture
+         // printf( "Cannot open file %s\n", "data.txt" );
+            return;
+        }
+        fprintf(inputFile,"%s\n", temoin);
+        fprintf(inputFile,"%.2f\n", myTemp.interieure);// ecriture de la consigne dans le fichier consigne.txt
+        fprintf(inputFile,"%.2f", myTemp.exterieure);
+        fclose( inputFile ); // ferme le fichier
+        remove(".verrouData");
+    }
+    
 }
