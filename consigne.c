32c91144094dc004a4747d1862449a0970f24446
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-03-09 17:21:06 +0100

    nouvelle version de consigne

--- consigne.c
+++ consigne.c
@@ -8,17 +8,18 @@ float thermostat_f=0;
    FILE *verrouConsigne;
  
    verrouConsigne = fopen(".verrouConsigne","r");
+   // Verification si le verrou existe
    if (verrouConsigne != NULL){
          thermostat_f = thermostatPrec_f;
    }else{
             
             verrouConsigne = fopen(".verrouConsigne","w");//Création de fichier .verrouconsigne
-   inputFile = fopen("consigne.txt", "r");
+            inputFile = fopen("consigne.txt", "r");
             if ( inputFile == NULL) { // teste l'ouverture
            // printf( "Cannot open file %s\n", "consigne.txt" );
             exit( 0 );
             }
-            fscanf(inputFile,"%f", &thermostat_f);
+            fscanf(inputFile,"%f", &thermostat_f);// Lecture de la consigne dans le fichier consigne.txt
             fclose( inputFile ); // ferme le fichier
             fclose( verrouConsigne );
       }
