25b2b2973719e12ca08fb7f4a4eff4194d9b774f
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-03-09 15:02:28 +0100

    consigne
    Finalisation de la fonction consigne.

--- consigne.c
+++ consigne.c
@@ -1,21 +1,33 @@
 #include "consigne.h"
 
-   float consigne(float thermostatPrec_f)
-   {
-   
-      float thermostat_f=0;
-      FILE *inputFile;
-      inputFile = fopen("consigne.txt", "r");
-      if ( inputFile == NULL) { // teste l'ouverture
-            printf( "Cannot open file %s\n", "consigne.txt" );
-            exit( 0 );
-      }
-      fscanf(inputFile,"%f", &thermostat_f);
-      
 
-      fclose( inputFile ); // ferme le fichier
+float consigne(float thermostatPrec_f)
+{
+float thermostat_f=0;
+   FILE *inputFile;
+   FILE *verrouConsigne;
+ 
+   verrouConsigne = fopen(".verrouConsigne","r");
+   if (verrouConsigne != NULL){
+         thermostat_f = thermostatPrec_f;
+   }else{
+            
+            verrouConsigne = fopen(".verrouConsigne","w");//Création de fichier .verrouconsigne
+   inputFile = fopen("consigne.txt", "r");
+            if ( inputFile == NULL) { // teste l'ouverture
+           // printf( "Cannot open file %s\n", "consigne.txt" );
+            exit( 0 );
+            }
+            fscanf(inputFile,"%f", &thermostat_f);
+            fclose( inputFile ); // ferme le fichier
+            fclose( verrouConsigne );
+      }
    
+
+   
+      remove(".verrouConsigne");
      
       return thermostat_f;
-   
-   }
+
+}
+
