ed20e63f3ee5a42e5d5a5363ef1a3a1a71736690
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2026-03-09 17:33:28 +0100

    visualisationC

--- visualisationC.c
+++ visualisationC.c
@@ -3,9 +3,28 @@
 #include <math.h>
 #include "visualisationC.h"
 
-
-   void visualisationC(float puissance_f)
-   {
-            	
-   
-   }
+void visualisationC(float puissance_f){
+    FILE * lockFile;
+    FILE * data;
+    lockFile = fopen(".verrouData","r");
+    if (lockFile == NULL){
+        lockFile = fopen(".verrouData","w");
+        data = fopen("data.txt","w");
+        if (data){
+            if (puissance_f==0){
+                fprintf(data,"false");
+            }
+            else {
+                if (puissance_f>0) {
+                    fprintf(data,"true");
+                }
+            }
+            fclose(data);
+        }
+        fclose(lockFile);
+        remove(".verrouData");
+    }
+    else {
+        fclose(lockFile);
+    }
+}
