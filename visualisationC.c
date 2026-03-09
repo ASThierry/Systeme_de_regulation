4f15101b3326059826366de3ae8e81cba5116457
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-03-09 22:34:46 +0100

    Finalisation de la fonction visualisationC.c

--- visualisationC.c
+++ visualisationC.c
@@ -4,27 +4,52 @@
 #include "visualisationC.h"
 
 void visualisationC(float puissance_f){
-    FILE * lockFile;
+    FILE * verrouData;
     FILE * data;
-    lockFile = fopen(".verrouData","r");
-    if (lockFile == NULL){
-        lockFile = fopen(".verrouData","w");
+    char ligne[25];
+    float text=0.0;
+    float tint=0.0;
+    verrouData = fopen(".verrouData","r");
+    
+    if (verrouData != NULL){// Présence du verrou
+        fclose(verrouData);
+        return;
+    }
+    // Création d'un vérrou
+    verrouData = fopen(".verrouData","w");
+    fclose( verrouData );
+    data = fopen("data.txt","r");
+      if (data !=NULL)
+      {
+        // On récupéré les donnée dans le fichier data.
+        fgets(ligne,sizeof(ligne),data);// Sauter la permier ligne.
+        
+        fgets(ligne,sizeof(ligne),data);
+        text=atof(ligne);
+
+        fgets(ligne,sizeof(ligne),data);
+        tint=atof(ligne);
+        fclose(data);
+      }
+      
+        
+        // on doit récupéré la valeur de la  consigne et celle de la température intérieur
+        
+        
         data = fopen("data.txt","w");
-        if (data){
-            if (puissance_f==0){
-                fprintf(data,"false");
+        if (data!=NULL){
+
+            if (puissance_f > 0){
+                fprintf(data,"true\n");
+            }else{
+                fprintf(data,"false\n");
+                
             }
-            else {
-                if (puissance_f>0) {
-                    fprintf(data,"true");
-                }
-            }
+            fprintf(data,"%.2f\n",text);
+            fprintf(data,"%.2f",tint);
             fclose(data);
         }
-        fclose(lockFile);
+        
         remove(".verrouData");
+    
     }
-    else {
-        fclose(lockFile);
-    }
-}
