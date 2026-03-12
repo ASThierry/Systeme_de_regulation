3ca4508237a4d2dc90d0e69624c5b6e603e14f4c
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2026-03-12 16:34:40 +0100

    commentaires + PID

--- main_sim.c
+++ main_sim.c
@@ -4,9 +4,8 @@
 #include<unistd.h>
 
 #include "simulateur.h"
-
-#include "consigne.h"
 #include "regulation.h"
+#include "consigne.h"
 #include "visualisationC.h"
 #include "visualisationT.h"
 
@@ -18,38 +17,50 @@ int main() {
 
     float cmd = 0; // initialisation de la commande du chauffage à 0%
     struct simParam_s *monSimulateur = simConstruct(temperature); // creation du simulateur, puissance intialisée
+
+    //initialise le tableau qui stocke les températures et une variable qui représente sa longueur
     int nT = 50;
     float tabT[50];
 
+    //initialise la consigne et sa valeur précédente
     float consi = 10;
     float consi_avant = 10;
 
+    //cet index permet de déterminer le taux de remplissage du tableau afin de déterminer quand le décalage doit commencer
     int i = 0;
     int k=0;
 
     while (1) {
         temperature = simCalc(cmd,monSimulateur); // calcul de la température grâce à la commande en % du chauffage via simCalc
 
+
+        //mets à jour la consigne dans consigne.txt en gardant trace de la consigne précédente
         consi_avant = consi;
         consi = consigne(consi_avant);
 
+
+        //arrête le programme quand la consigne est 5
         if (consi == 5) {
             cmd=0;
             break;
         }
 
+        //remplis le tableau de températures
         if (i < nT - 1) {
             tabT[i] = temperature.interieure;
             i++;
-        } else {
+        }
+        //le tableau est plein, les valeurs sont donc décalées vers la gauche
+        else {
         for (int j = 0; j < nT - 1; j++) {
                 tabT[j] = tabT[j + 1];
         }
         tabT[nT - 1] = temperature.interieure;
     }
+        //effectue la régulation de la puissance avec la méthode de Ziegler-Nichols
+        cmd = regulationPID(consi,tabT,nT,0.6*1000,2/3*DELTAT,0.125*(3*DELTAT));
 
-        cmd = regulationTest(2, consi, tabT, nT);
-
+        //écrit les températures et le témions dans data.txt
         visualisationC(cmd);
         visualisationT(temperature);
 
--- main_usb.c
+++ main_usb.c
@@ -22,17 +22,23 @@ int main() {
 
     float cmd = 0; // initialisation de la commande du chauffage à 0%
 
+    //initialise le tableau qui stocke les températures et une variable qui représente sa longueur
     int nT = 50;
     float tabT[50];
+
+
     FT_HANDLE ftHandle;
     FT_STATUS ftStatus;
+
+    //initialise la consigne et sa valeur précédente
     float consi = 10;
     float consi_avant = 10;
 
+    //cet index permet de déterminer le taux de remplissage du tableau afin de déterminer quand le décalage doit commencer
     int i = 0;
     int k=0;
    
-
+    //ouvre l'accès au port  USB et définit ses caractéristiques
     ftStatus = FT_Open(0, &ftHandle);
 
     FT_SetBaudRate(ftHandle, 115200); 
@@ -46,29 +52,37 @@ int main() {
     }
     else {
         while (1) {
+            //récupère les températures de la carte
             temperature = releve(ftHandle);
 
+            //mets à jour la consigne dans consigne.txt en gardant trace de la consigne précédente
             consi_avant = consi;
             consi = consigne(consi_avant);
 
+            //arrête le programme quand la consigne est 5
             if (consi == 5) {
                 cmd=0;
                 break;
             }
 
+            //remplis le tableau de températures
             if (i < nT - 1) {
                 tabT[i] = temperature.interieure;
                 i++;
             } else {
+
+                //le tableau est plein, les valeurs sont donc décalées vers la gauche
                 for (int j = 0; j < nT - 1; j++) {
                     tabT[j] = tabT[j + 1];
                 }
                 tabT[nT - 1] = temperature.interieure;
             }
 
+            //effectue la régulation de la puissance avec la méthode de Ziegler-Nichols et la transmets ensuite vers la carte
             cmd = regulationTest(1, consi, tabT, nT);
             commande(cmd,ftHandle);
 
+            //écrit les températures et le témions dans data.txt
             visualisationC(cmd);
             visualisationT(temperature);
 
